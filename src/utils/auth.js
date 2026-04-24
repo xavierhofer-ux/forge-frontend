8508d5e5dc9657851c7660875ea629b867b3e05b
xavierhofer-ux <xavierhofer@gmail.com>
2026-04-24 19:21:12 -0400

    Add src/utils/auth.js

---
+++ src/utils/auth.js
@@ -0,0 +1,33 @@
+export const getAuthToken = () => {
+  return localStorage.getItem('authToken');
+};
+
+export const setAuthToken = (token) => {
+  localStorage.setItem('authToken', token);
+};
+
+export const removeAuthToken = () => {
+  localStorage.removeItem('authToken');
+};
+
+export const getUser = () => {
+  const user = localStorage.getItem('user');
+  return user ? JSON.parse(user) : null;
+};
+
+export const setUser = (user) => {
+  localStorage.setItem('user', JSON.stringify(user));
+};
+
+export const removeUser = () => {
+  localStorage.removeItem('user');
+};
+
+export const isAuthenticated = () => {
+  return !!getAuthToken();
+};
+
+export const logout = () => {
+  removeAuthToken();
+  removeUser();
+};
