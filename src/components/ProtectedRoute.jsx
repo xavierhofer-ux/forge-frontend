e5ea0b0626d7aad98f2f3ffd707b92c7064b9cb2
xavierhofer-ux <xavierhofer@gmail.com>
2026-04-24 19:21:17 -0400

    Add src/components/ProtectedRoute.jsx

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { isAuthenticated } from '../utils/auth';
+
+export default function ProtectedRoute({ children }) {
+  if (!isAuthenticated()) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+}
